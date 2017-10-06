46d9519d2bc4b4efbe37a1c15040bf62a54b4dfb
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2017-10-06 07:42:13 -0600

    added stuff to java donut project

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -1,10 +1,69 @@
 package useful.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import useful.model.Donut;
+import useful.view.PopupDisplay;
+
 public class ToolController
 {
-
+	private List<Donut> donutList; //Declares the list
+	private PopupDisplay display;
+	
+	public ToolController()
+	{
+		donutList = new ArrayList<Donut>(); //Initializes the list
+		display = new PopupDisplay();
+	}
+	
+	
 	public void start()
 	{
+		Donut temp = new Donut(); //Creates the item "Donut"
 		
+		donutList.add(temp); 
+		fillTheList();
+		showTheList();
+	}
+	
+	private void showTheList()
+	{
+		String favorite = "Custard filled";
+		
+		for(int index = 0;  index < donutList.size(); index += 1)
+		{
+			String currentFlavor = donutList.get(index).getFlavor();
+			
+			Donut currentDonut = donutList.get(index);
+			String flavor = currentDonut.getFlavor();
+			
+			if (currentFlavor.equals(favorite))
+			{
+				for (int woop = 0; woop < 5; woop += 1)
+				{
+					display.displayText("The best flavor ever!!!!!");
+				}	
+			}
+			for ( int currentLetterIndex = 0; currentLetterIndex < currentFlavor.length(); currentLetterIndex += 1)
+			{
+				display.displayText(currentFlavor.substring(currentLetterIndex, currentLetterIndex + 1));
+			}
+			display.displayText(donutList.get(index).toString());
+		}
+	}
+	
+	private void fillTheList()
+	{
+		Donut jellyFilled = new Donut("jelly filled");
+		Donut pinkFrostedSprinkled = new Donut("pink frosted sprinkled");
+		Donut custardFilled = new Donut("custard filled");
+		Donut mapleBar = new Donut("maple bar");
+		Donut chocolateOldfashion = new Donut("chocolate old fashion");
+		
+		donutList.add(jellyFilled);
+		donutList.add(pinkFrostedSprinkled);
+		donutList.add(custardFilled);
+		donutList.add(mapleBar);
+		donutList.add(chocolateOldfashion);
 	}
 }
--- src/useful/model/Donut.java
+++ src/useful/model/Donut.java
@@ -2,5 +2,46 @@ package useful.model;
 
 public class Donut
 {
-
+	private boolean hasSprinkles;
+	private boolean isGlazed;
+	private String shape;
+	private String flavor;
+	private int holeCount;
+	
+	public Donut()
+	{
+		  this.flavor = "plain";
+		  this.shape = "round";
+		  this.holeCount = 1;
+		  this.hasSprinkles = false;
+		  this.isGlazed = false;
+	}
+	
+	public Donut(String flavor)
+	{
+		this();
+		this.flavor = flavor;
+	}
+	
+	public String toString()
+	{
+		String description = "this donut is a " + flavor + " donut";
+		
+		return description;
+	}
+	
+	public String getShape()
+	{
+		return shape;
+	}
+	
+	public void setShape(String shape)
+	{
+		this.shape = shape;
+	}
+	
+	public String getFlavor()
+	{
+		return flavor;
+	}
 }
